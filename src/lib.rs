a0101415d070b64ea082586f662bffb64f1d98b0
Kevan Hollbach <khollbach@gmail.com>
2023-05-21 15:22:50 -0700

    1-5

--- src/lib.rs
+++ src/lib.rs
@@ -148,4 +148,17 @@ mod tests {
             }
         }
     }
+
+    #[test]
+    fn problem_1_5() -> Result<()> {
+        let input = b"Burning 'em, if you ain't quick and nimble\n\
+            I go crazy when I hear a cymbal";
+        let key = b"ICE";
+
+        let encoded = xor_with_key(input, key);
+        let expected_hex = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+        assert_eq!(hex::encode(encoded), expected_hex);
+
+        Ok(())
+    }
 }
