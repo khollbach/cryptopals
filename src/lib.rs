48033f0d80e7c3324b0aba6cfd178e1b64efa7f4
Kevan Hollbach <khollbach@gmail.com>
2023-06-01 10:59:12 -0700

    Uncomment and refactor 1-4

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 #![cfg(test)]
 
 use std::{
-    collections::{HashSet, HashMap},
+    collections::{HashMap, HashSet},
     fs::File,
     io::{self, prelude::*, BufReader},
     iter::zip,
     path::PathBuf,
+    rc::Rc,
     str,
 };
 
@@ -16,14 +17,14 @@ use openssl::symm::{decrypt, Cipher};
 
 #[test]
 fn problem_1_1() -> Result<()> {
-    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+    let input_hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let expected_b64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
-    let bytes = hex::decode(input)?;
+    let bytes = hex::decode(input_hex)?;
     let base64 = base64_encode(&bytes);
-    assert_eq!(base64, expected);
+    assert_eq!(base64, expected_b64);
 
-    dbg!(hex_to_utf8(input)?);
+    eprintln!("{}", str::from_utf8(&bytes)?);
 
     Ok(())
 }
@@ -33,26 +34,23 @@ fn base64_encode(bytes: impl AsRef<[u8]>) -> String {
 }
 
 fn base64_decode(bytes: impl AsRef<[u8]>) -> Result<Vec<u8>> {
-    Ok(BASE64_STANDARD.decode(bytes)?)
-}
-
-fn hex_to_utf8(hex: &str) -> Result<String> {
-    let bytes = hex::decode(hex)?;
-    let s = String::from_utf8(bytes)?;
-    Ok(s)
+    let decoded = BASE64_STANDARD.decode(bytes)?;
+    Ok(decoded)
 }
 
 #[test]
 fn problem_1_2() -> Result<()> {
-    let s = "1c0111001f010100061a024b53535009181c";
-    let t = "686974207468652062756c6c277320657965";
-    let expected = "746865206b696420646f6e277420706c6179";
+    let input_hex = "1c0111001f010100061a024b53535009181c";
+    let key_hex = "686974207468652062756c6c277320657965";
+    let expected_hex = "746865206b696420646f6e277420706c6179";
 
-    let xor = xor_with_key(&hex::decode(s)?, &hex::decode(t)?);
-    assert_eq!(hex::encode(xor), expected);
+    let input = hex::decode(input_hex)?;
+    let key = hex::decode(key_hex)?;
+    let answer = xor_with_key(&input, &key);
+    assert_eq!(hex::encode(&answer), expected_hex);
 
-    dbg!(hex_to_utf8(t)?);
-    dbg!(hex_to_utf8(expected)?);
+    eprintln!("{}", str::from_utf8(&key)?);
+    eprintln!("{}", str::from_utf8(&answer)?);
 
     Ok(())
 }
@@ -64,33 +62,44 @@ fn xor_with_key(bytes: &[u8], key: &[u8]) -> Vec<u8> {
 
 #[test]
 fn problem_1_3() -> Result<()> {
-    let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-    let expected = "Q29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg";
+    let input_hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
 
-    let bytes = hex::decode(input)?;
-    let key = best_key(&bytes);
-    let decoded = xor_with_key(&bytes, &[key]);
-    let text = String::from_utf8(decoded.clone())?;
+    let input = hex::decode(input_hex)?;
+    let key = best_key(&input);
+    let decoded = xor_with_key(&input, &[key]);
+    let text = str::from_utf8(&decoded)?;
+    assert!(text.contains("bacon"));
 
-    dbg!(&text);
-    assert_eq!(base64_encode(&decoded), expected);
+    eprintln!("{text}");
 
     Ok(())
 }
 
-/// Helper function for problem 1-3.
-fn score(bytes: &[u8], key: u8) -> u32 {
-    let guess = xor_with_key(&bytes, &[key]);
-    bytes_score(&guess)
+/// Assume that `encoded_bytes` is english text that has been xor'd with a
+/// single-byte key.
+///
+/// Guess the best key to decode it, by trying all possible keys, and looking at
+/// letter frequencies in the decoded text.
+fn best_key(encoded_bytes: &[u8]) -> u8 {
+    (0..=u8::MAX)
+        .max_by_key(|&key| key_score(encoded_bytes, key))
+        .unwrap()
+}
+
+/// Helper function for `best_key`.
+fn key_score(encoded_bytes: &[u8], key: u8) -> u32 {
+    let maybe_text = xor_with_key(&encoded_bytes, &[key]);
+    text_score(&maybe_text)
 }
 
 /// Score `s` based on letter frequencies.
 ///
 /// English text should have a higher score than random noise.
-fn bytes_score(s: &[u8]) -> u32 {
+fn text_score(s: &[u8]) -> u32 {
     s.iter().copied().map(letter_score).sum()
 }
 
+/// Helper function for `text_score`.
 fn letter_score(byte: u8) -> u32 {
     match byte.to_ascii_uppercase() {
         b' ' => 150,
@@ -127,37 +136,31 @@ fn letter_score(byte: u8) -> u32 {
 #[test]
 fn problem_1_4() -> Result<()> {
     let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("inputs/1-4");
-    let _file = File::open(path)?;
+    let file = File::open(path)?;
 
-    // let mut err = anyhow::Ok(());
-    // let (best_score, answer) = BufReader::new(file)
-    //     .lines()
-    //     .map(|line| Ok(hex::decode(&line?)?))
-    //     .scan(&mut err, ok)
-    //     .map(Rc::new)
-    //     .cartesian_product(0..=u8::MAX)
-    //     .filter_map(|(bytes, key)| try_decode(&bytes, key))
-    //     .max_by_key(|&(score, _)| score)
-    //     .context("some (line, key) pair must produce ascii bytes")?;
-    // err?;
+    let mut err = anyhow::Ok(());
+    let (encoded_bytes, key) = BufReader::new(file)
+        .lines()
+        .map(|line| Ok(hex::decode(&line?)?))
+        .scan(&mut err, ok)
+        .map(Rc::new)
+        .cartesian_product(0..=u8::MAX)
+        .max_by_key(|(bytes, key)| key_score(&bytes, *key))
+        .context("file must be non-empty")?;
+    err?;
 
-    // dbg!(best_score, &answer);
+    let decoded = xor_with_key(&encoded_bytes, &[key]);
+    let text = str::from_utf8(&decoded)?;
+    assert!(text.contains("party"));
 
-    // let expected_base64 = "Tm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmcK";
-    // assert_eq!(base64_encode(&answer), expected_base64);
+    eprintln!("{text}");
 
     Ok(())
 }
 
-fn best_key(encoded_bytes: &[u8]) -> u8 {
-    (0..=u8::MAX)
-        .max_by_key(|&key| score(encoded_bytes, key))
-        .unwrap()
-}
-
 /// Like `result.ok()` but doesn't discard the error.
 ///
-/// Designed to be used with `Iterator::scan`.
+/// Designed to be used with `Iterator::scan`, to stop on the first error.
 fn ok<T, E>(err: &mut &mut Result<(), E>, item: Result<T, E>) -> Option<T> {
     match item {
         Ok(item) => Some(item),
